84a36214cc1f653df59ce71dd1a3b01ec963a468
Wonder <samuel.nai@yahoo.com>
2022-06-21 13:53:28 +0000

    display restaurants under restaurants section of home tab

---
+++ lib/home/homepage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:zone/widgets/restaurant_container.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("restaurants").doc("restaurant").collection("restaurantList").snapshots(),
+      builder: (context, snapshot) {
+        return !snapshot.hasData? const Text('Please Wait')
+          : ListView.builder(
+            padding: const EdgeInsets.only(top:10),
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, index) {
+            var restaurants =snapshot.data?.docs[index];
+              return Column(
+                children: [
+                  RestaurantContainer(name: restaurants!['restaurantName'], image: restaurants['restaurantImage'], rating: restaurants['restaurantRating']),
+                  Container(
+                    padding: const EdgeInsets.only(left: 5, right:5),
+                    width: MediaQuery.of(context).size.width*0.5,
+                    child: const Divider(
+                      color: Colors.black, 
+                      thickness: 2, 
+                    )
+                  )
+                ]
+              );
+            },
+          );
+      }
+    );
+  }
+}
