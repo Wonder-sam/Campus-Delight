2d125fe917798d8eda3200773a60765162758188
wonder <samuel.nai@yahoo.com>
2023-05-14 19:09:54 +0000

    edit UI

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class WelcomeScreen extends StatelessWidget{
-  WelcomeScreen({Key? key}): super(key: key);
+  const WelcomeScreen({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context){
