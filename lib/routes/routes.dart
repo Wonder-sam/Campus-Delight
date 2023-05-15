fdbec3e11f4fc8bf63184b75919b5fe0ff825ef0
wonder <samuel.nai@yahoo.com>
2023-05-15 01:57:11 +0000

    minor change

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,12 @@
 import 'package:get/get.dart';
 import 'package:zone/controllers/bottom_navigation.dart';
-import 'package:zone/login.dart';
+import 'package:zone/screens/login.dart';
 import 'package:zone/screens/welcome.dart';
 
 class RouterHelper {
   static List<GetPage> routes=[
     GetPage(name: '/welcome', page: ()=> const WelcomeScreen()),
-    GetPage(name: '/login', page: ()=> const LoginPage()),
+    GetPage(name: '/login', page: ()=> LoginPage()),
     GetPage(name: '/bottomNavigator', page: ()=> BottomNavigator())
   ];
 
