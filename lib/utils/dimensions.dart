8ee5637b2e0e51d8fd243de846f3bd35f9d08c32
wonder <samuel.nai@yahoo.com>
2023-07-03 18:11:59 +0000

    add new dimension

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,65 +1,67 @@
 import 'package:flutter/material.dart';
 
 class ScreenDimensions {
-
-  double dim_05 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.05;
+  double dim_05(BuildContext context) {
+    return MediaQuery.of(context).size.width * .05;
   }
 
- double dim_10 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.1;
+  double dim_10(BuildContext context) {
+    return MediaQuery.of(context).size.width * .1;
   }
 
-  double dim_10H (BuildContext context) {
-    return MediaQuery.of(context).size.height *.1;
+  double dim_10H(BuildContext context) {
+    return MediaQuery.of(context).size.height * .1;
   }
 
-  double dim_15 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.15;
+  double dim_15(BuildContext context) {
+    return MediaQuery.of(context).size.width * .15;
   }
 
-  double dim_15H (BuildContext context) {
-    return MediaQuery.of(context).size.height *.15;
+  double dim_15H(BuildContext context) {
+    return MediaQuery.of(context).size.height * .15;
   }
 
-   double dim_20 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.2;
+  double dim_20(BuildContext context) {
+    return MediaQuery.of(context).size.width * .2;
   }
 
-  double dim_25 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.25;
+  double dim_25(BuildContext context) {
+    return MediaQuery.of(context).size.width * .25;
   }
 
-   double dim_30 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.3;
+  double dim_30(BuildContext context) {
+    return MediaQuery.of(context).size.width * .3;
   }
 
-   double dim_40 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.4;
+  double dim_40(BuildContext context) {
+    return MediaQuery.of(context).size.width * .4;
   }
 
-  double dim_50 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.5;
+  double dim_50(BuildContext context) {
+    return MediaQuery.of(context).size.width * .5;
   }
 
-   double dim_60 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.6;
+  double dim_60(BuildContext context) {
+    return MediaQuery.of(context).size.width * .6;
   }
 
-   double dim_70 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.7;
+  double dim_65(BuildContext context) {
+    return MediaQuery.of(context).size.width * .65;
   }
 
-   double dim_80 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.8;
+  double dim_70(BuildContext context) {
+    return MediaQuery.of(context).size.width * .7;
   }
 
-   double dim_90 (BuildContext context) {
-    return MediaQuery.of(context).size.width *.9;
+  double dim_80(BuildContext context) {
+    return MediaQuery.of(context).size.width * .8;
   }
 
-   double dim_100 (BuildContext context) {
+  double dim_90(BuildContext context) {
+    return MediaQuery.of(context).size.width * .9;
+  }
+
+  double dim_100(BuildContext context) {
     return MediaQuery.of(context).size.width;
   }
-  
 }
