d2a3967289cca2e98e86cc62aeef510f3bb2807d
wonder <samuel.nai@yahoo.com>
2023-07-08 14:01:10 +0000

    change default theme to light

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zone/utils/color_theme.dart';
 
-final themeProvider = StateProvider<String>((ref) => "dark");
+final themeProvider = StateProvider<String>((ref) => "light");
 
 final selectThemeProvider = Provider<Map<String, dynamic>>((ref) {
   final themeType = ref.watch(themeProvider);
