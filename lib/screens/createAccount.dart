8b63948199322f1111e33c660a273780c3ef8226
wonder <samuel.nai@yahoo.com>
2023-07-08 12:46:01 +0000

    implement adding user info to created account on firebase

--- lib/screens/createAccount.dart
+++ lib/screens/createAccount.dart
@@ -2,16 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:intl_phone_field/phone_number.dart';
 import 'package:zone/controllers/bottom_navigation.dart';
+import 'package:zone/controllers/theme_controller.dart';
+import 'package:zone/firebase/firebase_functions.dart';
 import 'package:zone/utils/dimensions.dart';
+
+final lastNameProvider = StateProvider((ref) => "");
+final firstNameProvider = StateProvider((ref) => "");
+final phoneProvider = StateProvider((ref) => "");
+final TextEditingController phoneController = TextEditingController();
+
+void handleLastName(String text, WidgetRef ref) {
+  ref.read(lastNameProvider.notifier).state = text;
+}
+
+void handleFirstName(String text, WidgetRef ref) {
+  ref.read(firstNameProvider.notifier).state = text;
+}
+
+void handlePhone(PhoneNumber phone, WidgetRef ref) {
+  if (phone.number.startsWith('0')) {
+    print("here");
+    ref.read(phoneProvider.notifier).state = phone.countryCode + phone.number.substring(1);
+    phoneController.text = phone.number.substring(1);
+  } else {
+    ref.read(phoneProvider.notifier).state = phone.completeNumber;
+  }
+  print(ref.read(phoneProvider.notifier).state);
+}
 
 class CreateAccount extends ConsumerWidget {
   const CreateAccount({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final lastName = ref.watch(lastNameProvider);
+    final firstName = ref.watch(firstNameProvider);
+    final phone = ref.watch(phoneProvider);
+    Map<String, dynamic> theme = ref.watch(selectThemeProvider);
+
+    void handleCreateAccount() async {
+      if (lastName != '' && firstName != "" && phoneController.text != "") {
+        bool success = await createAccount(firstName, lastName, phone);
+        print(success);
+        if (success) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const BottomNavigator(),
+            ),
+          );
+        }
+      }
+    }
+
     return Scaffold(
       appBar: null,
+      backgroundColor: theme["background"],
       body: SingleChildScrollView(
         padding: EdgeInsets.only(
           left: ScreenDimensions().dim_10(context),
@@ -33,45 +80,55 @@ class CreateAccount extends ConsumerWidget {
               ),
             ),
             Container(
-                padding: EdgeInsets.only(
-                    top: ScreenDimensions().dim_05(context),
-                    bottom: MediaQuery.of(context).size.width * 0.03),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  "Additional information",
-                  style: TextStyle(color: Colors.grey, fontSize: 16),
-                )),
+              padding: EdgeInsets.only(
+                top: ScreenDimensions().dim_05(context),
+                bottom: MediaQuery.of(context).size.width * 0.03,
+              ),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "Additional information",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+            ),
             TextField(
-              onChanged: (text) => null,
+              onChanged: (text) => handleLastName(text, ref),
+              style: GoogleFonts.inter(
+                textStyle: TextStyle(
+                  color: theme['inputFieldLabel'],
+                ),
+              ),
               decoration: InputDecoration(
                 labelText: "Last Name",
                 floatingLabelAlignment: FloatingLabelAlignment.center,
-                labelStyle: const TextStyle(color: Colors.brown, fontSize: 14),
+                labelStyle: TextStyle(
+                  color: theme['inputFieldLabel'],
+                  fontSize: 14,
+                ),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.5,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.9,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.5,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
               ),
@@ -80,39 +137,44 @@ class CreateAccount extends ConsumerWidget {
               height: MediaQuery.of(context).size.height * 0.03,
             ),
             TextField(
-              onChanged: (text) => null,
+              onChanged: (text) => handleFirstName(text, ref),
+              style: GoogleFonts.inter(
+                textStyle: TextStyle(
+                  color: theme['inputFieldLabel'],
+                ),
+              ),
               decoration: InputDecoration(
                 labelText: "First Name(s)",
                 floatingLabelAlignment: FloatingLabelAlignment.center,
-                labelStyle: const TextStyle(
-                  color: Colors.brown,
+                labelStyle: TextStyle(
+                  color: theme['inputFieldLabel'],
                   fontSize: 14,
                 ),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.5,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.9,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.5,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
               ),
@@ -121,41 +183,52 @@ class CreateAccount extends ConsumerWidget {
               height: MediaQuery.of(context).size.height * 0.03,
             ),
             IntlPhoneField(
+              controller: phoneController,
+              disableLengthCheck: true,
               initialCountryCode: "GH",
+              style: GoogleFonts.inter(
+                textStyle: TextStyle(
+                  color: theme['inputFieldLabel'],
+                ),
+              ),
               decoration: InputDecoration(
                 counterText: '',
                 floatingLabelAlignment: FloatingLabelAlignment.start,
                 labelText: 'Phone Number',
+                labelStyle: TextStyle(
+                  color: theme['inputFieldLabel'],
+                  fontSize: 14,
+                ),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.5,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.9,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(
                     MediaQuery.of(context).size.width * 0.04,
                   ),
-                  borderSide: const BorderSide(
+                  borderSide: BorderSide(
                     width: 0.5,
-                    color: Colors.brown,
+                    color: theme['inputFieldBorder'],
                   ),
                 ),
               ),
               onChanged: (phone) {
-                print(phone.completeNumber);
+                handlePhone(phone, ref);
               },
               onCountryChanged: (country) {
                 print('Country changed to: ' + country.name);
@@ -179,8 +252,8 @@ class CreateAccount extends ConsumerWidget {
                     text: TextSpan(
                       text: "I agree to the ",
                       style: GoogleFonts.inter(
-                        textStyle: const TextStyle(
-                          color: Colors.black45,
+                        textStyle: TextStyle(
+                          color: theme['inputFieldLabel'],
                         ),
                       ),
                       children: [
@@ -195,8 +268,8 @@ class CreateAccount extends ConsumerWidget {
                         TextSpan(
                           text: "and ",
                           style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              color: Colors.black45,
+                            textStyle: TextStyle(
+                              color: theme['inputFieldLabel'],
                             ),
                           ),
                         ),
@@ -217,11 +290,7 @@ class CreateAccount extends ConsumerWidget {
             Padding(
               padding: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.05),
               child: MaterialButton(
-                onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const BottomNavigator(),
-                  ),
-                ),
+                onPressed: () => handleCreateAccount(),
                 color: Colors.brown,
                 minWidth: ScreenDimensions().dim_80(context),
                 height: ScreenDimensions().dim_15(context),
