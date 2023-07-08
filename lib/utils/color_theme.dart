1d5a65e8591fcbd9ff09b124191efb49986aa9c6
wonder <samuel.nai@yahoo.com>
2023-07-08 12:37:04 +0000

    add color themes

---
+++ lib/utils/color_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final Map<String, dynamic> lightTheme = {
+  "primary": Colors.brown,
+  "themeBackground": ColorScheme.fromSwatch(backgroundColor: Colors.white),
+  "background": Colors.white,
+  "inputFieldBorder": Colors.brown,
+  "inputFieldLabel": Colors.brown,
+  "texts": Colors.white70,
+  "buttonWrapper": Colors.grey.shade100
+};
+
+final Map<String, dynamic> darkTheme = {
+  "primary": Colors.brown,
+  "themeBackground": ColorScheme.fromSwatch(backgroundColor: Colors.black12),
+  "background": Colors.black54,
+  "inputFieldBorder": Colors.white,
+  "inputFieldLabel": Colors.grey,
+  "texts": Colors.white70,
+  "buttonWrapper": Colors.grey.shade900
+};
