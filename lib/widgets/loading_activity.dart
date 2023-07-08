25cf755f54879a12c1228faca7b3b628707ba765
wonder <samuel.nai@yahoo.com>
2023-07-08 13:54:49 +0000

    minor changes

--- lib/widgets/loading_activity.dart
+++ lib/widgets/loading_activity.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:zone/controllers/theme_controller.dart';
 import 'package:zone/utils/dimensions.dart';
 
@@ -10,22 +11,38 @@ class LoadingActivity extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Map<String, dynamic> theme = ref.watch(selectThemeProvider);
-    return Container(
-      color: theme['background'],
-      width: ScreenDimensions().dim_100(context),
-      height: ScreenDimensions().dim_100h(context),
-      child: Positioned.fill(
+    return Positioned.fill(
+      child: Container(
+        width: ScreenDimensions().dim_100(context),
+        height: ScreenDimensions().dim_100h(context),
+        decoration: BoxDecoration(
+          color: theme['background'],
+        ),
         child: Center(
           child: Container(
-            width: ScreenDimensions().dim_50(context),
-            height: ScreenDimensions().dim_50(context),
+            width: ScreenDimensions().dim_40(context),
+            height: ScreenDimensions().dim_40(context),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
               color: theme['inputFieldLabel'],
             ),
-            child: SpinKitCircle(
-              color: theme['primary'],
-              size: 50,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SpinKitCircle(
+                  color: theme['primary'],
+                  size: 50,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Please wait...",
+                  style: GoogleFonts.inter(
+                    textStyle: TextStyle(),
+                  ),
+                )
+              ],
             ),
           ),
         ),
