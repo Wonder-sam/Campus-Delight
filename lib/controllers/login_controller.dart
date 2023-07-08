0ccd1a4b6f7cb5d667427cbb038d6f9da91e5aaa
wonder <samuel.nai@yahoo.com>
2023-07-08 14:20:33 +0000

    create providers to handle states on login screen

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zone/controllers/theme_controller.dart';
+
+final usernameEmailProvider = StateProvider<String>((ref) => "");
+final passwordProvider = StateProvider<String>((ref) => "");
+final loginPasswordVisibilityProvider = StateProvider((ref) => false);
+
+final loginSelectPasswordIconProvider = Provider<Icon>((ref) {
+  Map<String, dynamic> theme = ref.watch(selectThemeProvider);
+  final passwordVisibilityState = ref.watch(loginPasswordVisibilityProvider);
+  if (passwordVisibilityState == false) {
+    return Icon(
+      Icons.visibility_outlined,
+      color: theme['inputFieldLabel'],
+    );
+  } else {
+    return Icon(
+      Icons.visibility_off_outlined,
+      color: theme['inputFieldLabel'],
+    );
+  }
+});
+
+void usernameEmailHandler(String text, WidgetRef ref) {
+  ref.read(usernameEmailProvider.notifier).state = text;
+}
+
+void passwordHandler(String text, WidgetRef ref) {
+  ref.read(passwordProvider.notifier).state = text;
+}
