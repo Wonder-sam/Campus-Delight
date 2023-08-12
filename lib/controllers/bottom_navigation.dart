e6777c63f99d25528d4827d3284c18b915904358
wonder <samuel.nai@yahoo.com>
2023-08-12 00:50:03 +0000

    minor changes

--- lib/controllers/bottom_navigation.dart
+++ lib/controllers/bottom_navigation.dart
@@ -2,20 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zone/controllers/bottom_nav_controller.dart';
 
-final selectedIndexProvider = StateProvider<int>((ref) => 0);
-
-void switchTab(int index, WidgetRef ref) {
-  ref.read(selectedIndexProvider.notifier).state = index;
-}
-
 class BottomNavigator extends ConsumerWidget {
   const BottomNavigator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedIndex = ref.watch(selectedIndexProvider);
+    final selectedTab = ref.watch(selectedTabProvider);
     return Scaffold(
-      body: BottomNavController().bottomTabPages[selectedIndex],
+      body: selectedTab,
       bottomNavigationBar: BottomNavigationBar(
         items: BottomNavController().getBottomTabs(),
         showSelectedLabels: true,
