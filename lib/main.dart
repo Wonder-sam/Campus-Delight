caf7bfdec96394fb8c2bb33cdf99a3d8fe7a4c78
wonder <samuel.nai@yahoo.com>
2023-09-10 22:27:18 +0000

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zone/controllers/bottom_navigation.dart';
+import 'package:zone/controllers/initial_screen_controller.dart';
 import 'package:zone/controllers/theme_controller.dart';
+import 'package:zone/home/homepage.dart';
 import 'package:zone/screens/welcome.dart';
 import 'firebase/firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -24,7 +27,8 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final theme = ref.read(selectThemeProvider);
+    final theme = ref.watch(selectThemeProvider);
+    final initialScreen = ref.watch(initialScreenProvider);
     return MaterialApp(
       title: 'Comfortite',
       theme: ThemeData(
@@ -32,7 +36,7 @@ class MyApp extends ConsumerWidget {
         colorScheme: theme["themeBackground"],
       ),
       debugShowCheckedModeBanner: false,
-      home: const WelcomeScreen(),
+      home: initialScreen,
     );
   }
 }
