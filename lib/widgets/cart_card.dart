6a7b7bfa9f8f221647fdee0dfb3315baf7a959ff
wonder <samuel.nai@yahoo.com>
2023-09-10 22:26:57 +0000

    add cart item component widget

---
+++ lib/widgets/cart_card.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CartCard extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      child: Row(
+        children: [
+          Container(
+            child: null,
+          ),
+          Column(
+            children: [],
+          ),
+        ],
+      ),
+    );
+  }
+}
