ca8d49b6e44fc579a390e3626cb782f63c03da7a
wonder <samuel.nai@yahoo.com>
2023-09-11 13:27:18 +0000

    edit bottom nav style

--- lib/controllers/bottom_navigation.dart
+++ lib/controllers/bottom_navigation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zone/controllers/bottom_nav_controller.dart';
+import 'package:zone/controllers/theme_controller.dart';
 
 class BottomNavigator extends ConsumerWidget {
   const BottomNavigator({Key? key}) : super(key: key);
@@ -9,16 +10,22 @@ class BottomNavigator extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedIndex = ref.watch(selectedIndexProvider);
     final selectedTab = ref.watch(selectedTabProvider);
+    final theme = ref.watch(selectThemeProvider);
+
     return Scaffold(
+      backgroundColor: theme['background'],
       body: selectedTab,
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: theme['background'],
         items: BottomNavController().getBottomTabs(),
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.brown,
+        selectedItemColor: theme['inputFieldLabel'],
         currentIndex: selectedIndex,
         onTap: (index) => switchTab(index, ref),
+        elevation: 0,
       ),
     );
   }
