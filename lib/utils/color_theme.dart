5bc1530aebace480c24fb1facc52afd91ee4b192
wonder <samuel.nai@yahoo.com>
2023-09-11 23:23:40 +0000

    add colors

--- lib/utils/color_theme.dart
+++ lib/utils/color_theme.dart
@@ -11,19 +11,21 @@ final Map<String, dynamic> lightTheme = {
   "buttonWrapper": Colors.grey.shade100,
   "activity": Colors.blue,
   "grey": Colors.grey.shade500,
-  "notify": Colors.red
+  "notify": Colors.red,
+  "brownWrap": Colors.brown
 };
 
 final Map<String, dynamic> darkTheme = {
   "primary": Colors.brown,
-  "themeBackground": ColorScheme.fromSwatch(backgroundColor: Colors.black12),
-  "background": Colors.black54,
+  "themeBackground": ColorScheme.fromSwatch(backgroundColor: Colors.black54),
+  "background": Colors.grey.shade900,
   "inputFieldBorder": Colors.white,
   "inputFieldLabel": Colors.white,
   "inputFieldText": Colors.grey,
   "texts": Colors.white70,
-  "buttonWrapper": Colors.grey.shade900,
+  "buttonWrapper": Colors.grey.shade700,
   "activity": Colors.white,
   "grey": Colors.grey.shade500,
-  "notify": Colors.white
+  "notify": Colors.white,
+  "brownWrap": Colors.grey.shade800
 };
